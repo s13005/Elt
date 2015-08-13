d5f765d9085b0064865abcdfb80655ac78ca1358
s13005 <s13005@std.it-college.ac.jp>
2015-08-13 14:22:59 +0900

    Initial commit

---
+++ app/src/main/java/com/example/s13005/elt/Game.java
@@ -0,0 +1,18 @@
+package com.example.s13005.elt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by s13005 on 15/08/12.
+ */
+public class Game extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game);
+
+
+    }
+}
